7dbc5cf8b017151c3493d8ed9b824c9c16ebf291
Simeon Petkov <petkov.moni@gmail.com>
2025-03-12 14:04:50 +0200

    Geolocation string parsing & query string preparation

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+const BASE_URL = 'https://api.bigdatacloud.net/data/reverse-geocode-client?';
 
+function coordinatesToLongLat(coordsString) {
+    let parts = coordsString.split(' ');
+    console.log(coordsString);
+
+    let latitude = parseInt(parts[0].split('°')[0]);
+    let latitudeSeconds = parseInt(parts[0].split('°')[1].split(`'`)) / 60.0;
+
+    latitude += latitudeSeconds;
+
+    let longitude = parseInt(parts[1].split('°')[0]);
+    let longitudeSeconds = parseInt(parts[1].split('°')[1].split(`'`)) / 60.0;
+
+    longitude += longitudeSeconds;
+
+    return {
+        latitude: latitude,
+        longitude: longitude
+    };
+}
+
+function convertToQueryString(base, coords, language) {    
+    if (language == undefined)
+        language = 'bg';
+
+    let params = new URLSearchParams({
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+        language: 'bg'
+    });
+
+    return base + params.toString();
+}
+
+function submitData() {
+    let content = document.getElementById('coordinates').value;
+
+    let coords = coordinatesToLongLat(content);
+    let queryString = convertToQueryString(BASE_URL, coords);
+
+    alert(queryString);
+}
